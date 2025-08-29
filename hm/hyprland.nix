1e101b89859f6d4c5f28b69f958230e510cfb202
Lev Koliadich <lev.koliadich@kyriba.com>
2025-08-29 16:45:44 +0200

    Fix option

--- hm/hyprland.nix
+++ hm/hyprland.nix
@@ -12,9 +12,9 @@ let
   keybindsByType = keybindType: builtins.filter ({ type, ... }: type == keybindType);
 in
 {
-  options.key.hyprland.enable = lib.mkEnableOption "Hyprland keybinds";
+  options.keybind.hyprland.enable = lib.mkEnableOption "Hyprland keybinds";
 
-  config = lib.mkIf config.key.hyprland.enable {
+  config = lib.mkIf config.keybind.hyprland.enable {
     wayland.windowManager.hyprland.settings = {
       bind = map renderKeybind (keybindsByType "PRESS" config.keybind.binds);
       binde = map renderKeybind (keybindsByType "HOLD" config.keybind.binds);
